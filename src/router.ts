d919e2b3f8561b52ed103597783a234b6fa4ff83
PAt <isc.patricio@gmail.com>
2025-02-27 13:42:20 -0600

    feat: enhance user model with TypeScript interface and update signup route to create user

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express';
+import User from './models/User';
 const router = Router();
 
 /** Autentication and register  */
-router.post('/auth/signup', (req, res) => {
+router.post('/auth/signup', async (req, res) => {
     console.log(`Signup ${JSON.stringify(req.body)}`);
-    res.send('Signup');
-    
+    await User.create(req.body);
+    res.json({message: 'User created'});
 });
 
 export default router;
